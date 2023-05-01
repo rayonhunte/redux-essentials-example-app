a809bcb5763dd94828db2d50dacb8504be27762d
Rayon Hunte <rayon.hunte@gmail.com>
2023-05-01 19:30:42 +0000

    start work

---
+++ src/features/post/postSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/tookkit';
+
+
+const initialState = []
+
+
+const postsSlice = createSlice({
+    name: 'post',
+    initialState,
+    reducers: {}
+})
+
+
+expost default postSclie.reducers
